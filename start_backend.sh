77e59748197eb528b180df035f98f9d17beab313
Ubuntu <ubuntu@ns559945.ip-54-39-103.net>
2025-10-06 21:02:34 +0000

    rer

--- start_backend.sh
+++ start_backend.sh
@@ -3,5 +3,6 @@ echo "Iniciando servidor backend de Python..."
 cd python_backend
 echo "Instalando dependencias..."
 pip install -r requirements.txt
-echo "Iniciando servidor Flask en http://localhost:5000"
 python3 ./app.py
+uvicorn app:app --host 0.0.0.0 --port 8000
+echo "Iniciando servidor Flask en http://localhost:5000"
